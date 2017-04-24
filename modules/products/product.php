f578539b5fa475021e42b5b898454308542533a7
mehul <mehul@chalkstreet.com>
2017-04-25 01:37:34 +0530

    inital product api

---
+++ modules/products/product.php
@@ -0,0 +1,42 @@
+<?php
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+function check_product_exist(Request $request, Response $response , $next)
+{
+  global $container;
+  $container['logger']->addInfo("check_product_exist");
+  $result = array("status"=>0,"msg"=>"<strong> Oh Snap !</strong>Something went wrong !!");
+  $request_params = $request->getParsedBody();
+
+  if(isset($request_params['product_id']))
+  {
+        $product_id=intval(trim($request_params['product_id']));
+        $product_status=1; // fetch only live products available
+        $sql="select `product_id` from `products` where `product_id`=:product_id and `product_status`=:product_status";
+        try{
+            // Get DB Object
+            $db = $container['db'];
+            $p_stmt= $db->prepare($sql);
+            $p_stmt->bindParam(":product_id",$product_id);
+            $p_stmt->bindParam(":product_status",$product_status);
+            $p_stmt->execute();
+            if($p_stmt->rowCount()>0)
+            {
+              $response=$next($request, $response);
+              return $response;
+            }
+            else {
+              $result['msg']='Sorry! Provided product is not avaiable..';
+              return $response->withStatus(404)->write(json_encode($result));
+            }
+        }catch(PDOException $e){
+          $result['msg']=$e->getMessage();
+          return $response->withStatus(403)->write(json_encode($result));
+        }
+  }
+  else {
+    $result['msg']='Please specify product ID..';
+    return $response->withStatus(403)->write(json_encode($result));
+  }
+}
+?>
